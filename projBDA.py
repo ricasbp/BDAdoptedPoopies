ede764864d169457bd5de8f3ed68c5ba3aaa636f
andre279m <mendes.andr843@gmail.com>
2022-11-13 22:15:56 +0000

    some bases to create the Database

--- projBDA.py
+++ projBDA.py
@@ -6,4 +6,30 @@ dc = pd.read_csv('disney-characters.csv')
 dd = pd.read_csv('disney-director.csv')
 dva = pd.read_csv('disney-voice-actors.csv')
 
+#-------------------------MONGODB-------------------------
+
+client = MongoClient()
+
+db = client.open_clusters_no_index#TODO
+cluster = db.cluster#TODO
+
+data_dc = dc.to_dict(orient = "records")
+data_dd = dd.to_dict(orient = "records")
+data_dva = dva.to_dict(orient = "records")
+
+cluster.insert_many(data_dc)
+cluster.insert_many(data_dd)
+cluster.insert_many(data_dva)
+
+
+
+#-------------------------SQLITE-------------------------
+
+conn = sqlite3.connect('open_clusters_no_index.db')#TODO
+c = conn.cursor()
+df.to_sql('clusters', conn)#TODO
+
+
+
+
 
