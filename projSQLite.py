eb1105ed37c5fbecf6f97f6f68d21f358fc33adf
fc55309 <fc55309@alunos.fc.ul.pt>
2022-11-21 00:37:30 +0000

    Fixed some errors e tentativa segundo querie

--- projSQLite.py
+++ projSQLite.py
@@ -41,6 +41,9 @@ for c in range(len(dc)):
     release_date = datetime.strptime(dc.loc[c][2], '%B %d, %Y')
     hero = dc.loc[c][3]
     villain = dc.loc[c][4]
+    if villain is None:   
+#TODO isto nao funciona
+        villain = "Nao existe" 
     song = dc.loc[c][5]
     try:
         cursor.execute(ins_qry_dc, (movie_title,release_date,hero,villain,song))
@@ -66,55 +69,63 @@ for va in range(len(dva)):
     vaArray = voice_actor.split("; ")
     for i in range(len(vaArray)):
         if vaArray[i] == "None":
-            vaArray[i] = None #TODO
+            vaArray[i] = None 
+#TODO
     movie = dva.loc[va][3]
     character = dva.loc[va][1]
     try:
         if voice_actor.__contains__(";"):
-            cursor.execute(ins_qry_dva, (vaArray[1],movie, character))
+            vaArray = voice_actor.split("; ")
+            for j in range(len(vaArray)):
+                cursor.execute(ins_qry_dva, (vaArray[j],movie, character))
         cursor.execute(ins_qry_dva, (vaArray[0],movie, character))
         conn.commit()
     except:
         print("error in operation")
         conn.rollback()
 
-sel_1_1 = "select voice_actor from voice_actors where movie = 'The Little Mermaid';"
+sel_a_1 = "select voice_actor from voice_actors where movie = 'The Little Mermaid';"
 cursor.execute(sel_1_1)
 
-rows_1_1 = cursor.fetchall()
+rows_a_1 = cursor.fetchall()
 
 #for r in rows_1_1:
 #    print(r)
 
-sel_1_2 = "select character from voice_actors group by character having count(character) > 1;"
+sel_a_2 = "select character from voice_actors group by character having count(character) > 1;"
 cursor.execute(sel_1_2)
 
-rows_1_2 = cursor.fetchall()
+rows_a_2 = cursor.fetchall()
 
 #for r in rows_1_2:
 #    print(r)
 
+# heroi do filme em que o nome do diretor começa com a letra P e tem mais de 5 atores
 #TODO ta bugado, nao aparece mais heros
-sel_2_1 = '''select hero 
+sel_b_1 = '''select hero 
 from characters
 inner join directors on directors.name = characters.movie_title and directors.director like 'B%'
 inner join voice_actors on voice_actors.movie = characters.movie_title group by voice_actors.movie having count(voice_actors.movie) > 5;'''
 
-cursor.execute(sel_2_1)
+cursor.execute(sel_b_1)
 
-rows_2_1 = cursor.fetchall()
+rows_b_1 = cursor.fetchall()
 
 #for r in rows_2_1:
 #    print(r)
 
-sel_2_2 = '''select hero 
-from characters
-inner join directors on directors.name = characters.movie_title and directors.director like 'B%'
-inner join voice_actors on voice_actors.movie = characters.movie_title group by voice_actors.movie having count(voice_actors.movie) > 5;'''
+# Todos os vilões que têm um voice_actor que não trabalhou com o Diretor "X"   
+# Nao funfa i dont' know why
+sel_b_2 = '''select c.villain, c.movie_title , director.name
+from characters c 
+left join voice_actors 
+on c.villain = voice_actors.character 
+left join director
+on c.movie_title = director.name
+and director.name == "Byron Howard" '''
+cursor.execute(sel_b_2)
 
-cursor.execute(sel_2_2)
+rows_b_2 = cursor.fetchall()
 
-rows_2_2 = cursor.fetchall()
-
-for r in rows_2_2:
+for r in rows_b_2:
     print(r)
