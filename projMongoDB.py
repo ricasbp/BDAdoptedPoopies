10ce8a8b36e5c767cbd8e338f4d27ea9ea491032
filipaalmendra <75578324+filipaalmendra@users.noreply.github.com>
2022-11-21 12:07:39 +0000

    mongo complex insert update
    need complex todo

--- projMongoDB.py
+++ projMongoDB.py
@@ -23,13 +23,36 @@ disneyC.insert_many(data_dc)
 disneyD.insert_many(data_dd)
 disneyVA.insert_many(data_dva)
 
-select1 = disneyVA.find({'movie':{'$eq':"The Little Mermaid"}})
+#select voice actors from the movie The Little Mermaid
+select1 = disneyVA.find({'movie':"The Little Mermaid"}, { 'voice-actor': 1})
 for s in select1:
-    print (s.get('voice_actor'), s.get('movie'))
-
+    print (s.get('voice-actor'))
+"""
+#select the characters that have more than one voice actor
 select2 = disneyVA.aggregate([{"$group":{_id:"$character", count:{"$sum":1}}},
                               {"$match":{"count":{"$gt":1}}}])
 
 for s1 in select2:
     print (s1.get('voice_actor'), s1.get('character'))
 
+#insert into directors "Stephen Hillenburg" para movie "Spongebob Squarepants"
+ins1 = disneyD.insert_one({'director':'Stephen Hillenburg','movie':'Spongebob Squarepants'})
+
+#update in characters in "The Jungle Book" villain from current to Balu
+upd = disneyC.update_one({'movie_title': 'The Jungle Book'},{"$set":{'villain':'Balu'}})
+"""
+
+#complex:
+#complexa1: heroi do filme em que nome diretor começa com letra B e tem mais de cinco atores
+
+#from disneyD where name: /^t/
+insc = disneyD.find({'name': /^B/},{'director':1})
+insc1 = disneyVA.aggregate([{"$group":{_id:"$movie", count:{"$sum":1}}},
+                              {"$match":{"count":{"$gt":5}}}])
+
+"""
+sel_b_1 = '''select hero 
+from characters
+inner join directors on directors.name = characters.movie_title and directors.director like 'B%'
+inner join voice_actors on voice_actors.movie = characters.movie_title group by voice_actors.movie having count(voice_actors.movie) > 5;'''
+"""
