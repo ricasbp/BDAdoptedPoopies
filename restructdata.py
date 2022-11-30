df3d316229ce24adcbd25f795eb575e83c52b5ee
andre279m <mendes.andr843@gmail.com>
2022-11-30 14:53:28 +0000

    completed datastruct

--- restructdata.py
+++ restructdata.py
@@ -7,8 +7,8 @@ def structCharacters():
         movie_title = dc.loc[c][1].replace('\n','')
         dc["movie_title"] = dc["movie_title"].replace([dc.loc[c][1]],movie_title)
 
-        release_date = datetime.strptime(dc.loc[c][2], '%B %d, %Y')
-        dc["release_date"] = dc["release_date"].replace([dc.loc[c][2]], release_date)
+        #release_date = datetime.strptime(dc.loc[c][2], '%B %d, %Y')
+        #dc["release_date"] = dc["release_date"].replace([dc.loc[c][2]], release_date)
 
     return dc
 
@@ -22,16 +22,15 @@ def structVoiceActors():
 
     voice_actor2 = []
 
-    for va in dva:
+    for va in dva.index:
         voice_actor2.append(None)
 
-    #TODO criar nova coluna no dataframe
-    dva = dva.assign( voice_actor2 = voice_actor2)
+    dva["voice_actor2"] = voice_actor2
 
     for va in range(len(dva)):
         voice_actor = dva.loc[va][2]
         vaArray = voice_actor.split("; ")
-
+        vaArray.append(None)
         dva['voice-actor'] = dva['voice-actor'].replace([dva.loc[va][2]],vaArray[0])
         dva['voice_actor2'] = dva['voice_actor2'].replace([dva.loc[va][4]],vaArray[1])
     return dva
